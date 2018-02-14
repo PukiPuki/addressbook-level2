80fa9353982aa41a706c168208c5cb2847b572f8
DF <dfwong5566@gmail.com>
2018-02-14 23:17:08 +0800

    Extracted common things into Contact

--- src/seedu/addressbook/ui/Formatter.java
+++ src/seedu/addressbook/ui/Formatter.java
@@ -1,25 +1,18 @@
 package seedu.addressbook.ui;
 
-import static seedu.addressbook.common.Messages.MESSAGE_GOODBYE;
-import static seedu.addressbook.common.Messages.MESSAGE_INIT_FAILED;
-import static seedu.addressbook.common.Messages.MESSAGE_PROGRAM_LAUNCH_ARGS_USAGE;
-import static seedu.addressbook.common.Messages.MESSAGE_USING_STORAGE_FILE;
-import static seedu.addressbook.common.Messages.MESSAGE_WELCOME;
+import seedu.addressbook.commands.CommandResult;
 
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.Scanner;
 
-import seedu.addressbook.commands.CommandResult;
-import seedu.addressbook.data.person.ReadOnlyPerson;
+import static seedu.addressbook.common.Messages.*;
 
 /**
  * Text UI of the application.
  */
-public class TextUi {
+public class Formatter {
 
     /** A decorative prefix added to the beginning of lines printed by AddressBook */
     private static final String LINE_PREFIX = "|| ";
@@ -42,59 +35,22 @@ public class TextUi {
     private final Scanner in;
     private final PrintStream out;
 
-    public TextUi() {
-        this(System.in, System.out);
-    }
-
-    public TextUi(InputStream in, PrintStream out) {
+    public Formatter(InputStream in, PrintStream out) {
         this.in = new Scanner(in);
         this.out = out;
     }
 
-    /**
-     * Returns true if the user input line should be ignored.
-     * Input should be ignored if it is parsed as a comment, is only whitespace, or is empty.
-     *
-     * @param rawInputLine full raw user input line.
-     * @return true if the entire user input line should be ignored.
-     */
-    private boolean shouldIgnore(String rawInputLine) {
-        return rawInputLine.trim().isEmpty() || isCommentLine(rawInputLine);
+    static String formatLinePrefix(String afterLinePrefix) {
+        return LINE_PREFIX.concat(afterLinePrefix);
     }
 
-    /**
-     * Returns true if the user input line is a comment line.
-     *
-     * @param rawInputLine full raw user input line.
-     * @return true if input line is a comment.
-     */
-    private boolean isCommentLine(String rawInputLine) {
-        return rawInputLine.trim().matches(COMMENT_LINE_FORMAT_REGEX);
+    static String formatCommandEntered(String fullInputLine) {
+       return tidyUp("[Command entered:" + fullInputLine + "]");
     }
 
-    /**
-     * Prompts for the command and reads the text entered by the user.
-     * Ignores empty, pure whitespace, and comment lines.
-     * Echos the command back to the user.
-     * @return command (full line) entered by the user
-     */
-    public String getUserCommand() {
-        out.print(LINE_PREFIX + "Enter command: ");
-        String fullInputLine = in.nextLine();
-
-        // silently consume all ignored lines
-        while (shouldIgnore(fullInputLine)) {
-            fullInputLine = in.nextLine();
-        }
-
-        showToUser("[Command entered:" + fullInputLine + "]");
-        return fullInputLine;
-    }
-
-
-    public void showWelcomeMessage(String version, String storageFilePath) {
+    static String formatWelcomeMessage(String version, String storageFilePath) {
         String storageFileInfo = String.format(MESSAGE_USING_STORAGE_FILE, storageFilePath);
-        showToUser(
+        return tidyUp(
                 DIVIDER,
                 DIVIDER,
                 MESSAGE_WELCOME,
@@ -104,49 +60,26 @@ public class TextUi {
                 DIVIDER);
     }
 
-    public void showGoodbyeMessage() {
-        showToUser(MESSAGE_GOODBYE, DIVIDER, DIVIDER);
+    static String formatMessage(String message) {
+        return tidyUp(message, DIVIDER, DIVIDER);
     }
 
-
-    public void showInitFailedMessage() {
-        showToUser(MESSAGE_INIT_FAILED, DIVIDER, DIVIDER);
-    }
-
-    /** Shows message(s) to the user */
-    public void showToUser(String... message) {
+    /** Formats messages */
+    static String tidyUp(String... message) {
+        StringBuilder formatted = new StringBuilder();
         for (String m : message) {
-            out.println(LINE_PREFIX + m.replace("\n", LS + LINE_PREFIX));
+            formatted.append(LINE_PREFIX + m.replace("\n", LS + LINE_PREFIX) + "\n");
         }
+        return formatted.toString();
     }
 
-    /**
-     * Shows the result of a command execution to the user. Includes additional formatting to demarcate different
-     * command execution segments.
-     */
-    public void showResultToUser(CommandResult result) {
-        final Optional<List<? extends ReadOnlyPerson>> resultPersons = result.getRelevantPersons();
-        if (resultPersons.isPresent()) {
-            showPersonListView(resultPersons.get());
-        }
-        showToUser(result.feedbackToUser, DIVIDER);
+    static String formatResultToUser(CommandResult result) {
+        return tidyUp(result.feedbackToUser, DIVIDER);
     }
 
-    /**
-     * Shows a list of persons to the user, formatted as an indexed list.
-     * Private contact details are hidden.
-     */
-    private void showPersonListView(List<? extends ReadOnlyPerson> persons) {
-        final List<String> formattedPersons = new ArrayList<>();
-        for (ReadOnlyPerson person : persons) {
-            formattedPersons.add(person.getAsTextHidePrivate());
-        }
-        showToUserAsIndexedList(formattedPersons);
-    }
-
-    /** Shows a list of strings to the user, formatted as an indexed list. */
-    private void showToUserAsIndexedList(List<String> list) {
-        showToUser(getIndexedListForViewing(list));
+    /** Formats a list of strings to the user, formatted as an indexed list. */
+    static String formatToUserAsIndexedList(List<String> list) {
+        return tidyUp(getIndexedListForViewing(list));
     }
 
     /** Formats a list of strings as a viewable indexed list. */
